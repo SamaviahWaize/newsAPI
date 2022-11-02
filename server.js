cc3dc45a78687ba5190761b1fc4c49e11aeb8734
SamaviahWaize <slitkitty1234@gmail.com>
2022-11-02 23:18:23 +0530

    news1

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
 const app=require('./app')
+const PORT = process.env.PORT||3000;
 const server=http.createServer(app);
-server.listen(3000,console.log("running"));
+server.listen(PORT,console.log("running"));
